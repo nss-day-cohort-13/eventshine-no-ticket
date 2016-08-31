f70902137db0507317fe1cfe6294f2476e76c77e
Michael Mead <michaelmead007@icloud.com>
2016-08-31 12:03:50 -0500

    view all events

--- eventshine/app/static/app/controllers/all_events.js
+++ eventshine/app/static/app/controllers/all_events.js
@@ -1,4 +1,7 @@
 angular.module('app')
 .controller('AllEventsCtrl', function($scope, $location, $http) {
-
+  $http.get('/all_events').then(res => {
+    console.log(res.data)
+    $scope.events = res.data
+  })
 })
